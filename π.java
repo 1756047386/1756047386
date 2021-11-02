8bc8291b893a9bf9325114ab701cb9ae19995c86
1756047386 <93073582+1756047386@users.noreply.github.com>
2021-11-02 22:19:12 +0800

    Add files via upload

---
+++ π.java
@@ -0,0 +1,28 @@
+public class π {
+
+
+
+    public static void main(String[] args) {
+
+
+        final double π = 3.14;
+
+
+        int r =5;
+
+
+        double s = π*r*r;
+
+
+        double c =2*π*r;
+
+
+        System.out.println("圆的面积为："+s);
+
+        System.out.println("圆的周长为："+c);
+
+
+
+    }
+
+}
